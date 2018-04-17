cd4a09fe50523803a164f6586c62ae80bfdc9599
Molan Ren <molan_ren@cooperators.ca>
2018-04-17 14:03:26 -0400

    Updated js and animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,44 +2,40 @@
 AOS.init();
 
 /* Back to Top */
-$(document).ready(function(){ 
-    $(window).scroll(function(){ 
-        if ($(this).scrollTop() > 100) { 
-            $('#scroll').fadeIn(); 
-        } else { 
-            $('#scroll').fadeOut(); 
-        } 
-    }); 
-    $('#scroll').click(function(){ 
-        $("html, body").animate({ scrollTop: 0 }, 600); 
-        return false; 
-    }); 
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
 });
 
 /* Interest buttons */
-$("#interest1").click(function(){
-  $("#interest1Content").slideToggle();
+function toggleText() {
   if ($(this).text() === '+') {
       $(this).text('–');
   } else {
       $(this).text('+');
   }
+}
+
+$("#interest1").click(function(){
+  $("#interest1Content").slideToggle();
+  toggleText.call(this);
 });
 
 $("#interest2").click(function(){
   $("#interest2Content").slideToggle();
-  if ($(this).text() === '+') {
-      $(this).text('–');
-  } else {
-      $(this).text('+');
-  }
+  toggleText.call(this);
 });
 
 $("#interest3").click(function(){
   $("#interest3Content").slideToggle();
-  if ($(this).text() === '+') {
-      $(this).text('–');
-  } else {
-      $(this).text('+');
-  }
+  toggleText.call(this);
 });
